92a674a4a78df03b375ceef43901d9ecaa5f7a23
kimmiseong <kimms000311@naver.com>
2025-09-11 09:38:16 +0900

    test commit

--- 2025.09/Spartan_SWEA/2weeks/0908/1974.py
+++ 2025.09/Spartan_SWEA/2weeks/0908/1974.py
@@ -17,7 +17,7 @@ def checkSudoku(M):
             # 이미 열에 같은 숫자가 있다면 0 반환 (스도쿠 조건 위배)
             if col_num[col]:
                 return 0
-            
+
             # 처음 등장한 숫자라면 해당 위치를 1로 기록
             row_num[row] = 1
             col_num[col] = 1
@@ -25,21 +25,22 @@ def checkSudoku(M):
             # 3x3 작은 정사각형 검사 (i, j가 3의 배수일 때만 검사 시작)
             if i % 3 == 0 and j % 3 == 0:
                 square = [0] * 10  # 3x3 내 숫자 사용 여부 체크
-                for r in range(i, i+3):      # 3행 순회
-                    for c in range(j, j+3):  # 3열 순회
+                for r in range(i, i + 3):  # 3행 순회
+                    for c in range(j, j + 3):  # 3열 순회
                         num = M[r][c]
                         # 이미 사용된 숫자면 0 반환
                         if square[num]:
                             return 0
                         # 처음 등장한 숫자는 기록
                         square[num] = 1
-    
+
     # 모든 조건을 통과하면 유효한 스도쿠 → 1 반환
     return 1
 
+
 # 테스트 케이스 개수 입력
 T = int(input())
-for tc in range(1, T+1):
+for tc in range(1, T + 1):
     # 9x9 스도쿠 보드 입력
     mat = [list(map(int, input().split())) for _ in range(9)]
 
