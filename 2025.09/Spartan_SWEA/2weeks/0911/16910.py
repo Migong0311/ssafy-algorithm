2769d26a93c5f1e9e3f2c00748995ab22a4a2064
kimmiseong <kimms000311@naver.com>
2025-09-12 08:54:37 +0900

    feat : 16910. 원 안의 점 완료

---
+++ 2025.09/Spartan_SWEA/2weeks/0911/16910.py
@@ -0,0 +1,19 @@
+import sys
+
+sys.stdin = open('test.txt', 'r')
+
+'''
+16910. 원 안의 점
+'''
+
+T = int(input())
+
+for t in range(1, T + 1):
+    N = int(input()) # 반지름
+    cnt = 0 # 문제 요구사항
+    for i in range(-N, N + 1): # x축 좌표
+        for j in range(-N, N + 1): # y축 좌표
+            if i * i + j * j <= N * N: #  x^2+y^2<=N^2인 격자점일 경우
+                cnt += 1 # 카운팅
+
+    print(f'#{t} {cnt}')
